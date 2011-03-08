a01885aa0ef6475358c8e3e78ca6fce002add0ef
Matt Todd <chiology@gmail.com>
2011-03-08 10:18:46 -0500

    Alert of iconv lib requirement

--- extconf.rb
+++ extconf.rb
@@ -2,8 +2,11 @@ require 'mkmf'
 
 dir_config("geoip")
 
-if have_library('GeoIP', 'GeoIP_record_by_ipnum') and have_header('GeoIPCity.h') and
-    (have_func('iconv_open', 'iconv.h') or have_library('iconv', 'iconv_open', 'iconv.h'))
+unless have_func('iconv_open', 'iconv.h') or have_library('iconv', 'iconv_open', 'iconv.h')
+  abort "you must have iconv library installed!"
+end
+
+if have_library('GeoIP', 'GeoIP_record_by_ipnum') and have_header('GeoIPCity.h')
   create_makefile('geoip')
 else
   abort("you must have geoip c library installed!")
