ad991015fe5af856cd2d5f24b472378b1aee4bf1
Andy Lindeman <andy@andylindeman.com>
2013-08-21 10:57:55 -0700

    Modernizes the test suite and use of bundler

--- test.rb
+++ test.rb
@@ -1,62 +1,57 @@
-require 'test/unit'
+require 'minitest/autorun'
 require File.dirname(__FILE__) + '/geoip'
-require 'rubygems'
-# require 'ruby-debug'
-# Debugger.start
 
 CITY_DB = ENV.fetch("CITY", '/usr/local/GeoIP/share/GeoIP/GeoLiteCity.dat')
 ORG_DB  = ENV.fetch("ORG",  '/usr/local/GeoIP/share/GeoIP/GeoIPOrg.dat')
 
-class Test::Unit::TestCase
-
+class Minitest::Test
   def assert_look_up(db, addr, field, value)
     h = db.look_up(addr)
     assert_equal value, h[field]
     h
   end
-
 end
 
-class GeoIPTest < Test::Unit::TestCase
-  
+class GeoIPTest < Minitest::Test
+
   def setup
     @ip = "24.24.24.24"
     @ipnum = 16777216*24 + 65536*24 + 256*24 + 24
-    
+
     @large_ip = "245.245.245.245"
     @large_ipnum = 16777216*245 + 65536*245 + 256*245 + 245
   end
-  
+
   # addr_to_num
-  
+
   def test_addr_to_num_converts_an_ip_to_an_ipnum
     assert_equal @ipnum, GeoIP.addr_to_num(@ip)
   end
-  
+
   def test_addr_to_num_converts_large_ips_to_an_ipnum_correctly
     assert_equal @large_ipnum, GeoIP.addr_to_num(@large_ip)
   end
-  
+
   def test_addr_to_num_expects_an_ip_string
-    assert_raises TypeError do 
-      GeoIP.addr_to_num(nil) 
+    assert_raises TypeError do
+      GeoIP.addr_to_num(nil)
     end
   end
-  
+
   def test_addr_to_num_returns_zero_for_an_illformed_ip_string
     assert_equal 0, GeoIP.addr_to_num("foo.bar")
   end
-  
+
   # num_to_addr
-  
+
   def test_num_to_addr_converts_an_ipnum_to_an_ip
     assert_equal @ip, GeoIP.num_to_addr(@ipnum)
   end
-  
+
   def test_num_to_addr_converts_large_ipnums_to_an_ip_correctly
     assert_equal @large_ip, GeoIP.num_to_addr(@large_ipnum)
   end
-  
+
   def test_num_to_addr_expects_a_numeric_ip
     assert_raises TypeError do
       GeoIP.num_to_addr(nil)
@@ -65,11 +60,11 @@ class GeoIPTest < Test::Unit::TestCase
       GeoIP.num_to_addr("foo.bar")
     end
   end
-  
+
 end
 
-class GeoIPCityTest < Test::Unit::TestCase
-  
+class GeoIPCityTest < Minitest::Test
+
   def setup
     ## Change me!
     @dbfile = CITY_DB
@@ -77,36 +72,36 @@ class GeoIPCityTest < Test::Unit::TestCase
 
   def test_construction_default
     db = GeoIP::City.new(@dbfile)
-    
-    assert_raises TypeError do 
-      db.look_up(nil) 
+
+    assert_raises TypeError do
+      db.look_up(nil)
     end
-    
+
     h = db.look_up('24.24.24.24')
     #debugger
     assert_kind_of Hash, h
-    assert_equal 'New York', h[:city]
+    assert_equal 'Deer Park', h[:city]
     assert_equal 'United States', h[:country_name]
   end
 
   def test_construction_index
     db = GeoIP::City.new(@dbfile, :index)
-    assert_look_up(db, '24.24.24.24', :city, 'New York')
+    assert_look_up(db, '24.24.24.24', :city, 'Deer Park')
   end
 
   def test_construction_filesystem
     db = GeoIP::City.new(@dbfile, :filesystem)
-    assert_look_up(db, '24.24.24.24', :city, 'New York')
+    assert_look_up(db, '24.24.24.24', :city, 'Deer Park')
   end
 
   def test_construction_memory
     db = GeoIP::City.new(@dbfile, :memory)
-    assert_look_up(db, '24.24.24.24', :city, 'New York')
+    assert_look_up(db, '24.24.24.24', :city, 'Deer Park')
   end
 
   def test_construction_filesystem_check
     db = GeoIP::City.new(@dbfile, :filesystem, true)
-    assert_look_up(db, '24.24.24.24', :city, 'New York')
+    assert_look_up(db, '24.24.24.24', :city, 'Deer Park')
   end
 
   def test_bad_db_file
@@ -122,8 +117,8 @@ class GeoIPCityTest < Test::Unit::TestCase
 
 end
 
-class GeoIPOrgTest < Test::Unit::TestCase
-  
+class GeoIPOrgTest < Minitest::Test
+
   def setup
     ## Change me!
     @dbfile = ORG_DB
@@ -131,11 +126,11 @@ class GeoIPOrgTest < Test::Unit::TestCase
 
   def test_construction_default
     db = GeoIP::Organization.new(@dbfile)
-    
-    assert_raises TypeError do 
-      db.look_up(nil) 
+
+    assert_raises TypeError do
+      db.look_up(nil)
     end
-    
+
     h = db.look_up('24.24.24.24')
     assert_kind_of Hash, h
     assert_equal 'Road Runner', h[:name]
